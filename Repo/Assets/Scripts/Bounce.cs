5aa2e8001c84f0713261c19380092527bc5e436a
knuuart <arttu.knuutinen@gmail.com>
2017-04-20 14:24:15 +0300

    Added Bounce.cs + cube

---
+++ Repo/Assets/Scripts/Bounce.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour {
+	Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void OnCollisionEnter(Collision c){
+		rb.AddForce (Vector3.up * 10f, ForceMode.Impulse);
+	}
+}
